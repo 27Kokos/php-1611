bced369b591b3e12eff06598a83323b9689603fd
kokos <vladosshemetov@gmail.com>
2023-02-11 19:10:17 +0300

    pp

--- php/classes/ArticleController.php
+++ php/classes/ArticleController.php
@@ -9,7 +9,17 @@ class ArticleController{
         global $mysqli;
         $result = $mysqli->query("SELECT * FROM articles WHERE id = $articleId");
         $row = $result->fetch_assoc();
-        $content = "<h1>".$row['title']."</h1><div>".$row['content']."</div>".$row['author'];
-        return $content;
+        return json_encode($row);
+    }
+
+    public static function deleteArticle($articleId){
+        global $mysqli; // Получаем доступ к переменной, которая отвечает за подключение к БД
+        $mysqli->query("DELETE FROM articles WHERE id=$articleId"); // Отправляем запрос на удаление к БД
+        header('Location: /'); // Переадресуем пользователя на главную страницу
+    }
+    public static function updateArticle($articleId, $title, $content, $author){
+        global $mysqli;
+        $mysqli->query("UPDATE articles SET title='$title',content='$content',author='$author' WHERE id=$articleId");
+        header("Location: /article/$articleId");
     }
 }
--- router.php
+++ router.php
@@ -1,7 +1,10 @@
 <?php
     session_start();
-    $requestURI = $_SERVER['REQUEST_URI'];
-    $method = $_SERVER['REQUEST_METHOD'];
+    // "/deleteArticle/2" -> $path = ["", "deleteArticle", "2"] -> if($path[1] == "deleteArticle"){выполняем инструкции удаления статьи}
+    // "/reg" -> $path = ["", "reg"] -> if($path[1] == "reg"){открывем страницу регистрации}
+    // "/login" -> $path = ["", "login"] -> if($path[1] == "login"){открываем страницу авторизации}
+    $requestURI = $_SERVER['REQUEST_URI']; // Получаем URI по которому запрошена страница
+    $method = $_SERVER['REQUEST_METHOD']; // Получаем метод запроса
     $path = explode('/', $requestURI);
     require_once('php/db.php');
     require_once('php/classes/UserController.php');
@@ -21,12 +24,19 @@
     }else if($path[1] == "addArticle" and $method=="POST"){
         ArticleController::addArticle($_POST['title'], $_POST['content'], $_POST['author']);
     } else if($path[1] == ""){}
-    else if($path[1] == "article"){
-        $content = ArticleController::getArticle($path[2]);
+    else if($path[1] == "article" and $method=="GET"){
+        $content = file_get_contents('article.html');
+    }else if($path[1] == "article" and $method=="POST"){
+        exit(ArticleController::getArticle($path[2]));
+    }else if($path[1] == "deleteArticle"){ // '/deleteArticle/2'
+        ArticleController::deleteArticle($path[2]); exit;
     }else if($path[1] == "getUserData"){
         UserController::getUserData();
-    }
-    else{
+    }else if($path[1] == "updateArticle" and $method=="GET"){
+        $content = file_get_contents('updateArticle.html');
+    }else if($path[1] == "updateArticle" and $method=="POST"){
+        ArticleController::updateArticle($_POST['id'], $_POST['title'], $_POST['content'], $_POST['author']);
+    }else{
         echo "Страница не найдена 404";
     }
     require_once('template.php');
