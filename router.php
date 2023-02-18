2d242b375c6d74df02e4de6d8ac3b31463a2169e
kokos <vladosshemetov@gmail.com>
2023-02-18 19:34:24 +0300

    1802

--- router.php
+++ router.php
@@ -9,22 +9,29 @@
     require_once('php/db.php');
     require_once('php/classes/UserController.php');
     require_once('php/classes/ArticleController.php');
-    if($path[1]=="reg" and $method=="GET"){
-        $content = file_get_contents("reg.php");
-    }else if($path[1]=="reg" and $method=="POST"){
-        UserController::reg($_POST['name'], $_POST['lastname'], $_POST['email'], $_POST['pass']);
-    }else if ($path[1]=="login" and $method=="GET"){
-        $content = file_get_contents('login.php');
-    }else if($path[1]=="login" and $method=="POST"){
-        UserController::login($_POST['email'], $_POST['pass']);
-    }else if($path[1]=="profile"){
-        $content = file_get_contents('profile.php');
-    }else if($path[1] == "addArticle" and $method=="GET"){
-        $content = file_get_contents('addArticle.php');
-    }else if($path[1] == "addArticle" and $method=="POST"){
-        ArticleController::addArticle($_POST['title'], $_POST['content'], $_POST['author']);
-    } else if($path[1] == ""){}
-    else if($path[1] == "article" and $method=="GET"){
+    require_once('php/classes/Route.php');
+
+    Route::get('/reg', function (){return file_get_contents("reg.php");});
+    Route::get('/login', function (){return file_get_contents('login.php');});
+    Route::get('/profile', function (){
+        if($_SESSION['id']){
+            return file_get_contents('profile.php');
+        }else{
+            header('Location: /login');
+        }
+    });
+    Route::get('/addArticle', function (){
+        if($_SESSION['id']){
+            return file_get_contents('addArticle.php');
+        }else{
+            header('Location: /login');
+        }
+    });
+    Route::post('/reg', function (){UserController::reg($_POST['name'], $_POST['lastname'], $_POST['email'], $_POST['pass']);});
+    Route::post('/login', function (){UserController::login($_POST['email'], $_POST['pass']);});
+    Route::post('/addArticle', function (){ArticleController::addArticle($_POST['title'], $_POST['content'], $_POST['author']);});
+
+    if($path[1] == "article" and $method=="GET"){
         $content = file_get_contents('article.html');
     }else if($path[1] == "article" and $method=="POST"){
         exit(ArticleController::getArticle($path[2]));
@@ -33,10 +40,20 @@
     }else if($path[1] == "getUserData"){
         UserController::getUserData();
     }else if($path[1] == "updateArticle" and $method=="GET"){
-        $content = file_get_contents('updateArticle.html');
+        if($_SESSION['id']){
+            $content = file_get_contents('updateArticle.html');
+        }else{
+            header('Location: /login');
+        }
     }else if($path[1] == "updateArticle" and $method=="POST"){
         ArticleController::updateArticle($_POST['id'], $_POST['title'], $_POST['content'], $_POST['author']);
+    }else if($path[1] == "getUsers" and $method=="GET"){
+        exit(UserController::getUsers());
+    }else if($path[1] == "users"){
+        $content = file_get_contents('users.html');
+    }else if($path[1] == "exit"){
+        UserController::logout();
     }else{
         echo "Страница не найдена 404";
     }
-    require_once('template.php');
+    //require_once('template.php');
